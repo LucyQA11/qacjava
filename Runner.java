c23ef132387049759399da606bc7310dc194c3a3
Lucy <Lucy@lucys-air.home>
2020-04-07 16:35:07 +0100

    file added

---
+++ Runner.java
@@ -0,0 +1,25 @@
+package com.qac.day1;
+
+public class Runner {
+
+	// This is a single line comment
+	/*
+	 * This is a multi-line comment
+	 */
+	public static void main(String args[]) {
+		int age = 23; // variables
+		// data type followed by name, ='s assigns then value
+		int height = 176;
+		String homeAdd = "123 example road, London, N50 3SA";
+		boolean bool = false;
+
+		System.out.println("Hello World");
+		System.out.println("I am " + age);
+		System.out.println("I am " + height + "cm");
+		System.out.println("I live at " + homeAdd);
+		System.out.println("The value of bool is " + bool);
+		System.out.println("my height is " + height + "my address is " + homeAdd + "the value of bool is " + bool);
+
+	}
+
+}
